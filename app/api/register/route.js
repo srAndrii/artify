5be344ebb3684e36658e1cadafb3443b2c170d4a
srAndrii <55akif5548@gmail.com>
2024-01-12 11:42:54 -0800

    Added User schema.Mongoose setup.Work with User registration

---
+++ app/api/register/route.js
@@ -0,0 +1,71 @@
+import { connectToDB } from "@mongodb/database";
+import User from "@models/User";
+import { NextResponse } from "next/server";
+import { hash } from "bcryptjs";
+import { writeFile } from "fs/promises";.
+
+// User Register
+export async function Post(req) {
+    try {
+        await connectToDB();
+
+        const data = await req.formData();
+
+        // Take information from the form
+        const username = data.get("username");
+        const email = data.get("email");
+        const password = data.get("password");
+        const file = data.get("profileImagePath");
+
+        if (!file) {
+            return NextResponse.json(
+                { message: "No file upload" },
+                { status: 400 }
+            );
+        }
+
+        const bytes = await file.arrayBuffer();
+        const buffer = Buffer.from(bytes);
+
+        const profileImagePath = `C:/Users/Andrii/Desktop/projects/artify/public/uploads/${file.name}`;
+        await writeFile(profileImagePath, buffer);
+
+        console.log(`open ${profileImagePath} to see the uploaded files`);
+
+        // Check if user exist
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return NextResponse.json(
+                { message: "User alredy exist!" },
+                { status: 409 }
+            );
+        }
+
+        // Hash the pasword
+        const saltRound = 10;
+        const hashedPasword = await hash(password, saltRound);
+
+        // Create a New User
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPasword,
+            profileImagePath: `uploads/${file.name}`,
+        });
+
+        //Save newUser
+        await newUser.save();
+
+        // Send success message
+        return NextResponse.json(
+            { message: "User registred successfuly!", user: newUser },
+            { status: 200 }
+        );
+    } catch (err) {
+        console.log(err);
+        return NextResponse.json(
+            { message: "Fail to create new User" },
+            { status: 500 }
+        );
+    }
+}
---
+++ models/User.js
@@ -0,0 +1,42 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: [true, "Username  alredy exist"],
+        required: [true, "Username is required"],
+    },
+    email: {
+        type: String,
+        unique: [true, "Email  alredy exist"],
+        required: [true, "Email is required"],
+    },
+    password: {
+        type: String,
+        required: [true, "Passsword is required"],
+    },
+    profileImagePath: {
+        type: String,
+        required: [true, "Profile image is required"],
+    },
+    wishlist: {
+        type: Array,
+        default: [],
+    },
+    cart: {
+        type: Array,
+        default: [],
+    },
+    order: {
+        type: Array,
+        default: [],
+    },
+    work: {
+        type: Array,
+        default: [],
+    },
+});
+
+const User = models.User || model("User", UserSchema);
+
+export default User;
