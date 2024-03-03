a3d403abd316c95cb23ff01fd2c619d732cd0986
srAndrii <55akif5548@gmail.com>
2024-03-02 16:55:20 -0800

    Remove deprecated mongoose connection options

--- mongodb/database.js
+++ mongodb/database.js
@@ -13,8 +13,6 @@ export const connectToDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URL, {
             dbName: "artify",
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
         });
 
         isConnected = true;
